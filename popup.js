c07a81d843a77738182a83e4f45d4206d3fca3b1
lucas <marcondeslucas979@gmail.com>
2023-03-02 01:58:41 -0300

    modified:   popup.js #	modified:   style.css

--- popup.js
+++ popup.js
@@ -7,16 +7,15 @@ function cadastrarArtigo(event) {
   const data = document.getElementById('input-data').value;
   const conteudo = document.getElementById('input-conteudo').value;
   const videoInput = document.getElementById('input-video');
-  const videoUrl = URL.createObjectURL(videoInput.files[0]); // obter a URL do arquivo selecionado
   const novoArtigo = {
     titulo: titulo,
     autor: autor,
     data: data,
     conteudo: conteudo,
-    videoUrl: videoUrl
+    video: videoInput.files[0]
   };
   artigos.push(novoArtigo);
-  localStorage.setItem('artigos', JSON.stringify(artigos)); // salvar os artigos no localStorage
+  localStorage.setItem('artigos', JSON.stringify(artigos));
   document.getElementById('form-artigo').reset();
   listarArtigos();
 }
@@ -31,28 +30,29 @@ function recuperarArtigos() {
 
 function excluirArtigo(index) {
   artigos.splice(index, 1);
-  localStorage.setItem('artigos', JSON.stringify(artigos)); // atualizar os artigos no localStorage
+  localStorage.setItem('artigos', JSON.stringify(artigos));
   listarArtigos();
 }
 
 function listarArtigos() {
   const lista = document.getElementById('lista-artigos');
   lista.innerHTML = '';
-  artigos.forEach((artigo, index) => { // adicionar o índice como parâmetro da função forEach
+  artigos.forEach((artigo, index) => {
     const item = document.createElement('li');
     const link = document.createElement('a');
-    const excluirBtn = document.createElement('button'); // criar o botão de excluir artigo
+    const excluirBtn = document.createElement('button');
     excluirBtn.innerHTML = 'Excluir';
     excluirBtn.addEventListener('click', () => {
       excluirArtigo(index);
     });
     link.setAttribute('href', '#');
     link.innerHTML = artigo.titulo;
-    link.addEventListener('click', () => {
-      exibirArtigo(artigo);
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      exibirArtigo(index);
     });
     item.appendChild(link);
-    item.appendChild(excluirBtn); // adicionar o botão de excluir artigo no item da lista
+    item.appendChild(excluirBtn);
     lista.appendChild(item);
   });
 }
@@ -60,82 +60,132 @@ function listarArtigos() {
 recuperarArtigos();
 
 document.getElementById('form-artigo').addEventListener('submit', cadastrarArtigo);
+
 document.getElementById('btn-buscar').addEventListener('click', () => {
   const termo = document.getElementById('input-consulta').value;
   buscarArtigos(termo);
 });
 
-function exibirArtigo(artigo) {
+function exibirArtigo(index) {
+  const artigo = artigos[index];
   let conteudo = `
     <h3>${artigo.titulo}</h3>
     <p>Autor: ${artigo.autor}</p>
     <p>Data: ${artigo.data}</p>
     <p>${artigo.conteudo}</p>
   `;
-  if (artigo.videoUrl) {
+  if (artigo.video) {
+    const videoUrl = URL.createObjectURL(artigo.video);
     conteudo += `
       <video id="videoArtigo" width="390" height="240" controls>
-        <source src="${artigo.videoUrl}" type="video/mp4">
+        <source src="${videoUrl}" type="video/mp4">
         Your browser does not support the video tag.
       </video>
-      `;
+      <button id="playBtn">Play</button>
+    `;
   }
-  conteudo += `
-    <button id="playBtn">Play</button>
-    <button id="btn-excluir-artigo">Excluir</button>
-  `;
-  document.getElementById('lista-artigos').innerHTML = conteudo;
+  document.getElementById('conteudo-artigo').innerHTML = conteudo;
 
-  if (artigo.videoUrl) {
-        const playBtn = document.getElementById('playBtn');
-            playBtn.addEventListener('click', () => {
+  if (artigo.video) {
+    const playBtn = document.getElementById('playBtn');
+    const video = document.getElementById('videoArtigo');
+    playBtn.addEventListener('click', () => {
+      video.play();
+    });
+  }
+
+  const btnExcluir = document.getElementById('btn-excluir-artigo');
+  btnExcluir.addEventListener('click', () => {
+    excluirArtigo(index);
+    document.getElementById('conteudo-artigo').innerHTML = '';
+  });
+}
+
+function buscarArtigos(termo) {
+    const lista = document.getElementById('lista-artigos');
+    lista.innerHTML = '';
+  
+    const artigosEncontrados = artigos.filter((artigo) => {
+      return artigo.titulo.toLowerCase().includes(termo.toLowerCase());
+    });
+  
+    artigosEncontrados.forEach((artigo, index) => {
+      const item = document.createElement('li');
+      const link = document.createElement('a');
+      const excluirBtn = document.createElement('button');
+  
+      excluirBtn.innerHTML = 'Excluir';
+  
+      excluirBtn.addEventListener('click', () => {
+        excluirArtigo(index);
+      });
+  
+      link.setAttribute('href', '#');
+      link.innerHTML = artigo.titulo;
+  
+      link.addEventListener('click', () => {
+        const conteudo = exibirArtigo(artigo);
+  
+        const artigoDiv = document.createElement('div');
+        artigoDiv.innerHTML = conteudo;
+        artigoDiv.setAttribute('id', 'artigo');
+  
+        lista.innerHTML = '';
+        lista.appendChild(artigoDiv);
+  
+        if (artigo.video) {
+          const playBtn = document.createElement('button');
+          playBtn.innerHTML = 'Play';
+          playBtn.setAttribute('id', 'playBtn');
+  
+          artigoDiv.appendChild(playBtn);
+  
+          playBtn.addEventListener('click', () => {
             const video = document.getElementById('videoArtigo');
             video.play();
-            });
-    const pauseBtn = document.getElementById('pauseBtn');
-        pauseBtn.addEventListener('click', () => {
-        const video = document.getElementById('videoArtigo');
-        video.pause();
+          });
+        }
+  
+        const btnExcluir = document.createElement('button');
+        btnExcluir.innerHTML = 'Excluir';
+        btnExcluir.setAttribute('id', 'btn-excluir-artigo');
+  
+        artigoDiv.appendChild(btnExcluir);
+  
+        btnExcluir.addEventListener('click', () => {
+          excluirArtigo(artigos.indexOf(artigo));
+        });
+      });
+  
+      item.appendChild(link);
+      item.appendChild(excluirBtn);
+      lista.appendChild(item);
     });
-    const muteBtn = document.getElementById('muteBtn');
-        muteBtn.addEventListener('click', () => {
-        const video = document.getElementById('videoArtigo');
-        video.muted = !video.muted;
-                if (video.muted) {
-                muteBtn.innerHTML = 'Unmute';
-                } else {
-                muteBtn.innerHTML = 'Mute';
-                }
-    });
- }
-    
-    const btnExcluir = document.getElementById('btn-excluir-artigo');
-    btnExcluir.addEventListener('click', () => {
-    excluirArtigo(artigo);
-    });
+  }
+  
+  function exibirArtigo(artigo) {
+    let conteudo = `<h3>${artigo.titulo}</h3>
+                    <p>Autor: ${artigo.autor}</p>
+                    <p>Data: ${artigo.data}</p>
+                    <p>${artigo.conteudo}</p>`;
+  
+    if (artigo.video) {
+      const videoUrl = URL.createObjectURL(artigo.video);
+      conteudo += `<video id="videoArtigo" width="390" height="240" controls>
+                      <source src="${videoUrl}" type="video/mp4">
+                      Your browser does not support the video tag.
+                   </video>`;
     }
-    
-    function buscarArtigos(termo) {
-            const lista = document.getElementById('lista-artigos');
-            lista.innerHTML = '';
-            const artigosEncontrados = artigos.filter((artigo) => {
-            return artigo.titulo.toLowerCase().includes(termo.toLowerCase());
-            });
-            artigosEncontrados.forEach((artigo, index) => {
-                const item = document.createElement('li');
-                const link = document.createElement('a');
-                const excluirBtn = document.createElement('button');
-                excluirBtn.innerHTML = 'Excluir';
-                excluirBtn.addEventListener('click', () => {
-                excluirArtigo(index);
-                });
-                    link.setAttribute('href', '#');
-                    link.innerHTML = artigo.titulo;
-                    link.addEventListener('click', () => {
-                    exibirArtigo(artigo);
-    });
-        item.appendChild(link);
-        item.appendChild(excluirBtn);
-        lista.appendChild(item);
-});
-}
+  
+    return conteudo;
+  }
+  
+  recuperarArtigos();
+  
+  document.getElementById('form-artigo').addEventListener('submit', cadastrarArtigo);
+  
+  document.getElementById('btn-buscar').addEventListener('click', () => {
+    const termo = document.getElementById('input-consulta').value;
+    buscarArtigos(termo);
+  });
+  
